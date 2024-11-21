1f26bbcee3705d6c341326591714fcd56a122dbf
bxcowo <a.flores.a@uni.pe>
2024-11-21 11:26:51 -0500

    Mejora y completitud de verificar historial

--- src/app/components/verificar-historial/verificar-historial.component.ts
+++ src/app/components/verificar-historial/verificar-historial.component.ts
@@ -1,13 +1,41 @@
 import { Component } from '@angular/core';
+import { VerificarHistorialService } from '../../services/verificar-historial.service';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-verificar-historial',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './verificar-historial.component.html',
   styleUrl: './verificar-historial.component.css'
 })
+
 export class VerificarHistorialComponent {
+  id: string = '';
+  isLoading: boolean = false;
+  isValid: boolean | null = null;
+
+  constructor(private verificarHistorialService: VerificarHistorialService, private router: Router) { }
+  
+  onSubmit() {
+    this.isLoading = true;
+    this.verificarHistorialService.search(this.id).subscribe(isValid => {
+      this.isLoading = false;
+      this.isValid = isValid;
+    });
+  }
+
+  onRetry(){
+    this.isValid = null;
+    this.id = '';
+  }
+
+  onNavigate(route: string){
+    this.router.navigate([route]);
+  }
+
   ngAfterViewInit() {
     let arrows = document.querySelectorAll(".arrow");
     arrows.forEach(arrow => {
